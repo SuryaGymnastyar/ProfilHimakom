420b440ddacda6155d592c98fa6c038797a665b9
SuryaGymnastyar <suryagymnastyarr@gmail.com>
2024-10-05 21:10:43 +0700

    Update profilhimakom.cpp

--- profilhimakom.cpp
+++ profilhimakom.cpp
@@ -1,7 +1,65 @@
 #include <iostream>
-using namespace std;
+using namespace std:
 
+void menu();
+void sejarahHima();
+void visiMisiHima();
+void strukturHima();
+void profilPresidium();
+void profilBansus();
+void bidangHima();
+void profilKader();
+void profilExster();
+void profilInter();
+void profilKeilmuan();
+void profilMedin();
+void biroHima();
+void profilKsr();
+void profilKwu();
+void keluar();
 
+void menu() {
+    cout << endl << " ** PROFIL HIMAKOM UNILA ** " << endl;
+    cout << "1. Sejarah Singkat Himakom" << endl;
+    cout << "2. Visi Misi Himakom" << endl;
+    cout << "3. Struktur Organisasi Himakom" << endl;
+    cout << "4. Keluar" << endl; 
+}
+
+void sejarahHima() {
+    cout << endl << "                                    ** Sejarah Singkat Himakom **                                    " << endl;
+    cout << endl << "Himakom terbentuk pada tahun 2012. Awalnya di Ilmu Komputer sendiri memiliki nama KOMIK (Komunitas Ilmu Komputer)" << endl;
+    cout << "dan saat itu masih di bawah naungan Himatika (Himpunan Mahasiswa Matematika). Awalnya di himakom terdapat" << endl;
+    cout << "4 bidang dan 2 biro. Sekarang kepengurusan himakom menjadi presidium, 1 badan khusus, 5 bidang, dan 2 biro." << endl;
+}
+
+void visiMisiHima(){
+    cout << endl << "                                   ** Visi Misi Himakom **                                   << endl << endl;
+    cout << ** Visi Himakom **;
+    cout << endl << "Menciptakan sebuah himpunan yang inklusif, inovatif, dan kompetitif" << endl;
+    cout << "dengan memperjuangkan perubahan positif dalam kepemimpinan, serta memberikan kesempatan" << endl;
+    cout << "bagi anggota untuk berkembang secara holistik dan membangun identitas yang kuat." << endl;
+    cout << endl << " ** Misi Himakom ** ";
+    cout << endl << "1.Mendorong inovasi dalam kepemimpinan untuk menciptakan perubahan yang signifikan" << endl;
+    cout << "dan memberikan manfaat maksimal bagi anggota himpunan." << endl;
+    cout << "2. Menjunjung tinggi nilai-nilai transparansi dan akuntabilitas dalam setiap keputusan dan tindakan." << endl;
+    cout << "3. Menjadi himpunan yang aktif dan positif dalam memecahkan masalah dan mendukung kegiatan akademik" << endl;
+    cout << "dan non-akademik di lingkungan jurusan maupun di luar jurusan." << endl;
+}
+
+void strukturHima(){
+    cout << endl << " * Terdapat Presidium, Badan Khusus, Bidang Hima dan Biro Hima * " << endl;
+    cout << "1. Presidium" << endl;
+    cout << "2. Badan Khusus" << endl;
+    cout << "3. Bidang Hima" << endl;
+    cout << "4. Biro Hima" << endl;
+}
+
+void profilPresidium (){
+    cout << "* Presidium *" << endl;
+    cout << "Presidium memiliki tugas yaitu pengkoordinasian kegiatan organisasi, memiliki wewenang dalam pembuatan keputusan bersama" << endl;
+    cout << "serta mewakili himpunan dalam berbagai forum. Presidium juga memiliki peran strategis menjaga stabilitas dan keberlanjutan organisasi." << endl;
+    cout << "Presidium juga mempunyai fungsi yaitu mengembangkan program kerja, memfasilitası komunikasi antar anggota, dan memimpin rapat organisasi." << endl;
 
 int main(){
 
